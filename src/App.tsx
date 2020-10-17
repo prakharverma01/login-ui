2f831509a851228b25a4781adf210adedbcaf09b
Prakhar Verma <prakhar.verma@hashedin.com>
2020-10-17 20:53:41 +0530

    adding landing to login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Login from './Components/Login'
 import RegisterNewAccount from './Components/RegisterNewAccount'
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import WorkInProgress from './Components/WorkInProgress';
 
 class App extends React.Component {
@@ -13,6 +13,7 @@ class App extends React.Component {
         <Route exact path="/login" component={Login}/>
         <Route exact path="/registration" component={RegisterNewAccount}/>
         <Route exact path="/wip" render = {() => <WorkInProgress/>}/>
+        <Redirect from='/' to='/login' />
       </div>
       );
   }
